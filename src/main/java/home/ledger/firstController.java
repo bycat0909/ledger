eceba74d480e2eff7b8ee61c8616bad9e2766f41
dev_daddy_choiYs <bycat0702@naver.com>
2023-11-14 20:55:04 +0900

    Initial commit

---
+++ src/main/java/home/ledger/firstController.java
@@ -0,0 +1,19 @@
+package home.ledger;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.HashMap;
+
+@RestController
+public class firstController {
+    @GetMapping("/test")
+    public Map<String, Object> test(){
+        Map<String, Object> returnData = new HashMap<>();
+
+        returnData.put("test", "data");
+        return returnData;
+    }
+}
