cfb6b95b748eea223fbf9fa2e0bd9400588501c8
dev_daddy_choiYs <bycat0702@naver.com>
2023-11-21 23:21:38 +0900

    commit

--- src/main/java/home/ledger/LedgerApplication.java
+++ src/main/java/home/ledger/LedgerApplication.java
@@ -3,11 +3,13 @@ package home.ledger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ @SpringBootApplication 하위 패키지로 컴포넌트 스캔을 함.
+ 그래서 해당 클래스의 위치가 중요함
+ */
 @SpringBootApplication
 public class LedgerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(LedgerApplication.class, args);
     }
-
 }
