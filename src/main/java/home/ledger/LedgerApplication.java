a660379d01117c6a9f5a88e69dfdae7667b305ca
dev_daddy_choiYs <bycat0702@naver.com>
2023-11-22 22:48:14 +0900

    commit

--- src/main/java/home/ledger/LedgerApplication.java
+++ src/main/java/home/ledger/LedgerApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /*
- @SpringBootApplication 하위 패키지로 컴포넌트 스캔을 함.
- 그래서 해당 클래스의 위치가 중요함
+ @SpringBootApplication 하위 패키지로 컴포넌트 스캔을 함.그래서 해당 클래스의 위치가 중요함
+ SpringBootApplication 어노테이션은 아래의 어노테이션을 함께 사용한 것과 같음
+ - SpringBootConfiguration
+ - ComprnentScan
+ - EnableAutoConfiguration
  */
 @SpringBootApplication
 public class LedgerApplication {
