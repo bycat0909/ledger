f758d16743d30623f04411f042be90c245681ff3
dev_daddy_choiYs <bycat0702@naver.com>
2023-12-05 22:34:29 +0900

    강의 정독중...

--- src/main/java/home/ledger/SampleRunner.java
+++ src/main/java/home/ledger/SampleRunner.java
@@ -14,7 +14,7 @@ public class SampleRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        //...
+        //......
         System.out.println(ysclass.getName());
     }
 }
