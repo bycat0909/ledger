514998a513d2878188bd2f11d1a9f142baf3b9bc
dev_daddy_choi <bycat0909@gamil.com>
2024-02-01 21:03:09 +0900

    T_USER_M 사용자 관련 테이블 JPA 업데이트 작업 진행중..

--- src/main/frontend/ledger/src/App.js
+++ src/main/frontend/ledger/src/App.js
@@ -3,17 +3,20 @@ import Topbar from "./compnent/topbar/Topbar"
 import Home from "./pages/home/Home"
 import './App.css'
 import UserList from "./pages/userList/userList"
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App(){
     return(
-        <div className="App">
+        <Router>
             <Topbar/>
             <div className="container">
                 <Sidebar/>
-                <Home/>
+                <Routes>
+                    <Route exact path="/" element={<Home/>}/>
+                    <Route path="/users" element={<UserList/>}/>
+                </Routes>
             </div>
-        </div>
+        </Router>
     )
 }
 
--- src/main/frontend/ledger/src/pages/userList/userList.jsx
+++ src/main/frontend/ledger/src/pages/userList/userList.jsx
@@ -1,6 +1,36 @@
 import React from 'react'
 import './userList.css'
 
+import {DataGrid} from "@mui/x-data-grid";
+
+const columns =[
+    {field : 'id', headerName : 'ID', width : 70},
+    {field : 'firstName', headerName : 'First Name', width : 130},
+    {field : 'lastName', headerName : 'Last Name', width : 130},
+    {field : 'age', headerName : 'Age', type: 'number', width : 90},
+    {field : 'fullName', headerName : 'Full Name', description:'설명', sortable:false ,width : 160},
+]
+
+const rows =[
+    {id:1, lastName:'Snow', firstName:'jon', age : 35},
+    {id:1, lastName:'Lannister', firstName:'Cersei', age : 35},
+    {id:1, lastName:'Lannister', firstName:'Jaime', age : 35},
+    {id:1, lastName:'Stark', firstName:null, age : 35},
+    {id:1, lastName:'Targaryen', firstName:'Ferrara', age : 35},
+    {id:1, lastName:'Frances', firstName:'Rossini', age : 35},
+    {id:1, lastName:'Roxie', firstName:'Harvey', age : 35},
+]
 export default function UserList(){
-    return <div className="userList"></div>
+    return (
+        <div className="userList">
+            <DataGrid
+                columns={columns}
+                rows={rows}
+                pageSize={5}
+                rowsPerPageOptions={[5]}
+                checkboxSelection
+            />
+        </div>
+    )
+
 }
