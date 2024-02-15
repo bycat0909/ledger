aa1000f0ff5c2d2c71bb0bb744dd1997119edaa6
dev_daddy_choi <bycat0909@gamil.com>
2024-02-15 23:38:43 +0900

    Hook 첫 사용부분까지 정리됨

--- src/main/frontend/ledger/src/pages/userList/userList.jsx
+++ src/main/frontend/ledger/src/pages/userList/userList.jsx
@@ -1,37 +1,12 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import {useState} from "react";
+
 import './userList.css'
 
 import {DataGrid} from "@mui/x-data-grid";
 import {DeleteOutline} from "@mui/icons-material";
 
-const columns =[
-    {field : 'id', headerName : 'ID', width : 70},
-    {
-        field : 'user', headerName : 'user', width : 130,
-        renderCell : (params) =>{
-            return (
-                <div className="userListUser">
-                    <img className="userListImg" src={params.row.avatar} alt=""/>
-                    {params.row.username}
-                </div>
-            )
-        },
-    },
-    {field : 'email', headerName : 'Email', width : 130},
-    {field : 'status', headerName : 'Status', width : 90},
-    {field : 'transaction', headerName : 'Transaction', description:'설명', sortable:false ,width : 160},
-    {field : 'action', headerName : 'Action', width : 150,
-        renderCell : ()=>{
-            return(
-                <>
-                    <dutton className ="userListEdit">Edit</dutton>
-                    <DeleteOutline className ='userListDelete'/>
-                </>
-            )
-        }
-    },
-
-]
 
 const rows =[
     {id:1, username:'Snow', avatar:'https://i.namu.wiki/i/zr_c5Elw2ABSiwGgDnyF-0LAF0HvJIiYyn_H374u6NZJ-Etso0RkdQD1cNC69EZ2QIQWo1k_OFFaO4N-huibx8TRBp8a1fwtvkGOO8zNNMln1h-9cVlHAJv1WwPxfzcaBixlcdL71O4fR308JeUVWw.webp' ,email:'Snow@gmail.com', status : 'active', transaction : '$120.00'},
@@ -50,10 +25,48 @@ const rows =[
     {id:14, username:'Cersei', avatar:'https://i.namu.wiki/i/zr_c5Elw2ABSiwGgDnyF-0LAF0HvJIiYyn_H374u6NZJ-Etso0RkdQD1cNC69EZ2QIQWo1k_OFFaO4N-huibx8TRBp8a1fwtvkGOO8zNNMln1h-9cVlHAJv1WwPxfzcaBixlcdL71O4fR308JeUVWw.webp',email:'Cersei@gmail.com', status : 'active', transaction : '$120.00'},
 ]
 export default function UserList(){
+    const[data, setData] = useState(rows)
+    const handleDelete = (id)=>{
+        setData(data.filter((item)=> item.id !== id))
+    }
+
+    const columns =[
+        {field : 'id', headerName : 'ID', width : 70},
+        {
+            field : 'user', headerName : 'user', width : 130,
+            renderCell : (params) =>{
+                return (
+                    <div className="userListUser">
+                        <img className="userListImg" src={params.row.avatar} alt=""/>
+                        {params.row.username}
+                    </div>
+                )
+            },
+        },
+        {field : 'email', headerName : 'Email', width : 130},
+        {field : 'status', headerName : 'Status', width : 90},
+        {field : 'transaction', headerName : 'Transaction', description:'설명', sortable:false ,width : 160},
+        {field : 'action', headerName : 'Action', width : 150,
+            renderCell : (params)=>{
+                return(
+                    <>
+                        <Link to={'/user/'+params.row.id}>
+                            <button className={"userListEdit"}>Edit</button>
+                        </Link>
+                        <DeleteOutline
+                            className ='userListDelete'
+                            onClick={()=> handleDelete(params.row.id)}
+                        />
+                    </>
+                )
+            }
+        },
+    ]
+
     return (
         <div className="userList">
             <DataGrid
-                rows={rows}
+                rows={data}
                 disableSelectionOnClick
                 columns={columns}
                 initialState={{
