30b647f8cfb03e9a7e452aad219410812160d35c
dev_daddy_choi <bycat0909@gamil.com>
2024-02-22 23:23:13 +0900

    사용자화면 디자인중

--- src/main/frontend/ledger/src/pages/user/User.jsx
+++ src/main/frontend/ledger/src/pages/user/User.jsx
@@ -2,5 +2,29 @@ import React from "react";
 import './User.css'
 
 export default function User(){
-    return <div className="user">user pages</div>
+    return (
+        <div className="user">
+            <div className="userTitleContainer">
+                <h1 className="userTitle">Edit User</h1>
+                <button className="userAddButton">Create</button>
+            </div>
+            <div className="userContainer">
+                <div className="userShow">
+                    <div className="userShowTop">
+                        <img
+                                src="/img/img.jpg"
+                                alt=""
+                                className="userShowImg"
+                        />
+                        <div className="userShowTopTitle">
+                            <span className="userShowUserName">최민성</span>
+                            <span className="userShowUserTitle">유치원생</span>
+                        </div>
+                    </div>
+                    <div className="userShowBottom"></div>
+                </div>
+                <div className="userUpdate">update</div>
+            </div>
+        </div>
+    )
 }
