6b0a84626939ace9cd2e68dc8c23712273e99356
bycat0909 <bycat0702@naver.com>
2024-02-27 16:55:17 +0900

    기본적인 개념은 정리됨

--- src/main/frontend/ledger/src/pages/user/User.jsx
+++ src/main/frontend/ledger/src/pages/user/User.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './User.css'
+import {CalendarToday, LocationOn, MailOutline, PermIdentity, PhoneIphone} from "@mui/icons-material";
 
 export default function User(){
     return (
@@ -21,7 +22,33 @@ export default function User(){
                             <span className="userShowUserTitle">유치원생</span>
                         </div>
                     </div>
-                    <div className="userShowBottom"></div>
+                    <div className="userShowBottom">
+                        <span className="userShowTitle">Account Details</span>
+                        <div className="userShowInfo">
+                            <PermIdentity className ="userShowIcon"/>
+                            <span className ="userShowIconTitle">hani99</span>
+                        </div>
+
+                        <div className="userShowInfo">
+                            <CalendarToday className ="userShowIcon"/>
+                            <span className ="userShowIconTitle">25.12.2000</span>
+                        </div>
+                        <span className="Contact Details"></span>
+                        <div className="userShowInfo">
+                            <PhoneIphone className ="userShowIcon"/>
+                            <span className ="userShowIconTitle">01091588840</span>
+                        </div>
+
+                        <div className="userShowInfo">
+                            <MailOutline className ="userShowIcon"/>
+                            <span className ="userShowIconTitle">bycat0702@naver.com</span>
+                        </div>
+
+                        <div className="userShowInfo">
+                            <LocationOn className ="userShowIcon"/>
+                            <span className ="userShowIconTitle">Sydeny | Australa</span>
+                        </div>
+                    </div>
                 </div>
                 <div className="userUpdate">update</div>
             </div>
