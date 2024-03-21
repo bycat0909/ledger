168332e09b926a07730099459ddc68250ce820bd
dev_daddy_choiYs <bycat0702@naver.com>
2024-03-21 23:14:51 +0900

    배경 컨테이너 정렬중

--- src/main/java/com/ledger/config/security/WebSecurityConfig.java
+++ src/main/java/com/ledger/config/security/WebSecurityConfig.java
@@ -1,6 +1,9 @@
 package com.ledger.config.security;
 
 
+
+
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,8 +11,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 /*
@@ -39,9 +45,22 @@ public class WebSecurityConfig extends WebSecurityConfiguration {
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults())
-                .formLogin(Customizer.withDefaults());
-
+                .formLogin((formLogin ->
+                        formLogin
+                                .usernameParameter("username")
+                                .passwordParameter("password")
+                                .defaultSuccessUrl("/", true)
+                        )
+                );
         return http.build();
     }
 
+    public UserDetailsService userDetailsService(){
+        //https://docs.spring.io/spring-security/reference/servlet/authentication/architecture.html 읽고 시큐리티 분석중..
+        UserBuilder users = User.withDefaultPasswordEncoder();
+
+        return new InMemoryUserDetailsManager(u)
+    }
 }
+
+
