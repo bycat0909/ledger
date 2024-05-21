128e1cab4ab6fcc758a8dd5580eaf9d68f05378f
dev_daddy_choiYs <bycat0702@naver.com>
2024-05-21 23:11:04 +0900

    스프링시큐리티 공부

--- src/main/java/com/ledger/config/security/service/CustomUserDetailService.java
+++ src/main/java/com/ledger/config/security/service/CustomUserDetailService.java
@@ -28,6 +28,7 @@ public class CustomUserDetailService implements UserDetailsService {
             UserDetails를 상속받은 UserVo를 Return하면 됨
             UserDetailsService를 implements받은 CustomUserDetailServiceimpl의
             loadUserByUsername 에서 사용자를 조회해서 리턴하면 됨.
+            Test
          */
 
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
